4027589b02feb102cae6a2c4b4c1f53d1ad0d81a
Grzegorz Nosek <grzegorz.nosek@sysdig.com>
2018-08-06 16:12:20 +0200

    getsockopt()/setsockopt() support
    Only SOL_SOCKET options are decoded for now, SO_ERROR values are mapped to errno

--- driver/ppm_fillers.h
+++ driver/ppm_fillers.h
@@ -50,6 +50,8 @@ along with sysdig.  If not, see <http://www.gnu.org/licenses/>.
 	FN(proc_startupdate_2)			\
 	FN(proc_startupdate_3)			\
 	FN(sys_socketpair_x)			\
+	FN(sys_setsockopt_x)			\
+	FN(sys_getsockopt_x)			\
 	FN(sys_connect_x)			\
 	FN(sys_accept4_e)			\
 	FN(sys_accept_x)			\
